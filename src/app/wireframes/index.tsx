b4c39eb7731443a995cbb0c4e29e1d5792a97f08
MFarao <165041359+MFarao@users.noreply.github.com>
2026-05-18 13:28:15 -0300

    Eliminacion de web

--- src/app/wireframes/index.tsx
+++
@@ -1,6 +0,0 @@
-export { HomeWireframe } from './HomeWireframe';
-export { LoginWireframe } from './LoginWireframe';
-export { DashboardWireframe } from './DashboardWireframe';
-export { AuctionsWireframe } from './AuctionsWireframe';
-export { LiveAuctionWireframe } from './LiveAuctionWireframe';
-export { WireframeGallery } from './WireframeGallery';
