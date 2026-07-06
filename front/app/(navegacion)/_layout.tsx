24b783742be3ecacf9df787c2f551f732a19f93d
Flor Ayala <flayala@uade.edu.ar>
2026-07-06 00:49:33 -0300

    Eliminado hamburguesa sin login

--- front/app/(navegacion)/_layout.tsx
+++ front/app/(navegacion)/_layout.tsx
@@ -181,7 +181,7 @@ export default function TabLayout() {
             resizeMode="contain"
           />
         ),
-        headerRight: () => <HamburgerMenu />,
+        headerRight: isAuthenticated ? () => <HamburgerMenu /> : undefined,
       }}
     >
       <Tabs.Screen
